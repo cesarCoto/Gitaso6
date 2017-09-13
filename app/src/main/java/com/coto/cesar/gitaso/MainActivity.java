1eeefa9bd10957dfa7b4e7716fa537b855165082
Cesar <cesar.cotobvb@gmai.com>
2017-09-12 20:26:38 -0500

    al

--- app/src/main/java/com/coto/cesar/gitaso/MainActivity.java
+++ app/src/main/java/com/coto/cesar/gitaso/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Perro", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Pero", Toast.LENGTH_SHORT).show();
     }
 }
